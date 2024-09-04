3ee228b2d4dead8ae5183ccf414a432022ef3a3b
Jedaiah Tantanella <jedanne143@gmail.com>
2024-09-04 10:02:09 -0400

    added middleware

--- server.js
+++ server.js
@@ -10,33 +10,19 @@ app.set('views' , path.join(__dirname , 'views'))
 
 //Middleware
 app.use(express.static(path.join(__dirname , 'public')))
+const logMiddleware = (req, res, next) => {
+    console.log(`Request [method: ${req.method}] [URL: ${req.url}]`)
+    next()
+}
+app.use(logMiddleware)
+app.use((req, res, next) => {
+    console.log(`Date and time of access: ${new Date()}`)
+    next()
+})
 
-
-const  plants = [
-    { 
-        Plant :"Serendipity Pink", 
-        Genus:"Alocasia", 
-        Price: "$300"
-    },
-    { 
-        Plant :"Thai Constellation", 
-        Genus:"Monstera", 
-        Price: "$100"
-    },
-    { 
-        Plant :"Adansonii Variegated", 
-        Genus:"Monstera", 
-        Price: "$100"
-    },
-    { 
-        Plant :"Micans Variegated", 
-        Genus:"Philodendron", 
-        Price: "$150"
-    }
-]
 // ROUTES
 app.get('/' , (req, res) => {
-    res.send("<h2>You are in the root directory</h2>")
+    res.send("<h2>You are in the root directory</h2><h3>Try '/home'. </h3>")
 }) 
 app.get('/home' , (req, res) => {
     res.render("homepage")
@@ -47,9 +33,9 @@ app.get('/new' , (req, res) => {
 app.get('/plants' , (req, res) => {
     res.render("index")
 }) 
-app.get('/r/:subreddit' , (req,res) => {
-    const {subreddit} = req.params
-    res.send(`<h2>You are browsing the ${subreddit} subreddit`)
+app.get('/p/:genus' , (req,res) => {
+    const {genus} = req.params
+    res.send(`<h2>You are browsing ${genus} plants`)
 })
 app.get("*", (req, res) => {
     res.status(404).send("Path doesn't exist")
