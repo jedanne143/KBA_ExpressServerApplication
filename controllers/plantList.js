486c2942dad80af474c388b2bbde41ec444672cb
Jedaiah Tantanella <jedanne143@gmail.com>
2024-09-04 12:55:50 -0400

    added plantList

---
+++ controllers/plantList.js
@@ -0,0 +1,120 @@
+const plantList = [
+    {
+        Plant: "Serendipity Pink",
+        Genus: "Alocasia",
+    },
+    {
+        Plant: "Blizzard",
+        Genus: "Alocasia",
+    },
+    {
+        Plant: "Sinuata Variegated",
+        Genus: "Alocasia",
+    },
+    {
+        Plant: "Dragon Scale",
+        Genus: "Alocasia",
+    },
+    {
+        Plant: "Odora",
+        Genus: "Alocasia",
+    },
+    {
+        Plant: "Silver Scale Variegated",
+        Genus: "Alocasia",
+    },
+    {
+        Plant: "Pink Bambino",
+        Genus: "Alocasia",
+    },
+    {
+        Plant: "Adansonii Tricolor",
+        Genus: "Monstera",
+    },
+    {
+        Plant: "Adansonii Mint",
+        Genus: "Monstera",
+    },
+    {
+        Plant: "Peru Variegated",
+        Genus: "Monstera",
+    },
+    {
+        Plant: "Thai Constellation",
+        Genus: "Monstera",
+    },
+    {
+        Plant: "Albo",
+        Genus: "Monstera",
+    },
+    {
+        Plant: "Micans Variegated",
+        Genus: "Philodendron",
+    },
+    {
+        Plant: "Pink Caramel Marble",
+        Genus: "Philodendron",
+    },
+    {
+        Plant: "Pink Princess",
+        Genus: "Philodendron",
+    },
+    {
+        Plant: "Tortum",
+        Genus: "Philodendron",
+    },
+    {
+        Plant: "Tetrasperma Variegated",
+        Genus: "Rhaphidophora",
+    },
+    {
+        Plant: "Florida Beauty Variegated",
+        Genus: "Philodendron",
+    },
+    {
+        Plant: "Linearis",
+        Genus: "Hoya",
+    },
+    {
+        Plant: "Joepii",
+        Genus: "Philodendron",
+    },
+    {
+        Plant: "Pinnatum Marble",
+        Genus: "Epipremnum",
+    },
+    {
+        Plant: "Global Green",
+        Genus: "Epipremnum",
+    },
+    {
+        Plant: "Black Raven",
+        Genus: "ZZ",
+    },
+    {
+        Plant: "Snake Plant Golden Flame",
+        Genus: "Dracaena",
+    },
+    {
+        Plant: "Snake Plant Cylindrica",
+        Genus: "Dracaena",
+    },
+    {
+        Plant: "Snake Plant Fernwood",
+        Genus: "Dracaena",
+    },
+    {
+        Plant: "Birkin",
+        Genus: "Philodendron",
+    },
+    {
+        Plant: "Red Borju",
+        Genus: "Aglaonema",
+    },
+    {
+        Plant: "Pictum Tricolor",
+        Genus: "Aglaonema",
+    }
+]
+
+module.exports = plantList;
--- server.js
+++ server.js
@@ -2,27 +2,32 @@ const express = require("express")
 const app = express()
 const path = require('path')
 const PORT = 5000
+const plantList = require('./controllers/plantList')
 
-// app.use(express.urlencoded({ extended}))
-// app.use(express.json())
+app.use(express.urlencoded({ extended : true}))
+app.use(express.json())
 app.set("view engine" , "ejs")
 app.set('views' , path.join(__dirname , 'views'))
 
 //Middleware
 app.use(express.static(path.join(__dirname , 'public')))
+//logs method and url
 const logMiddleware = (req, res, next) => {
     console.log(`Request [method: ${req.method}] [URL: ${req.url}]`)
     next()
 }
 app.use(logMiddleware)
+//logs date and time
 app.use((req, res, next) => {
     console.log(`Date and time of access: ${new Date()}`)
     next()
 })
 
+
+
 // ROUTES
 app.get('/' , (req, res) => {
-    res.send("<h2>You are in the root directory</h2><h3>Try '/home'. </h3>")
+    res.send("<h2>You are in the root directory</h2><h3>Try '/home'.</h3>")
 }) 
 app.get('/home' , (req, res) => {
     res.render("homepage")
@@ -31,9 +36,9 @@ app.get('/new' , (req, res) => {
     res.render("newplant")
 }) 
 app.get('/plants' , (req, res) => {
-    res.render("index")
+    res.render("index" , {plantList})
 }) 
-app.get('/p/:genus' , (req,res) => {
+app.get('/plants/:genus' , (req,res) => {
     const {genus} = req.params
     res.send(`<h2>You are browsing ${genus} plants`)
 })
